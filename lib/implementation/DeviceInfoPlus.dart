c9076f41e4744d7dc5d3382b2b3839b5dc9e7c01
Simone Anile <23080613+simusr2@users.noreply.github.com>
2022-01-04 21:59:43 +0100

    Add device info in InfoPage

--- lib/implementation/DeviceInfoPlus.dart
+++ lib/implementation/DeviceInfoPlus.dart
@@ -1,10 +1,13 @@
+import 'dart:io';
 import 'package:device_info_plus/device_info_plus.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 Future<String> getDeviceModel() async {
   final DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
   final BaseDeviceInfo baseDeviceInfo = await deviceInfo.deviceInfo;
-  final String baseDeviceInfoString = 'Unknown ${baseDeviceInfo.runtimeType}';
+  final String baseDeviceInfoString = 'Unknown (${baseDeviceInfo.runtimeType})';
   switch (baseDeviceInfo.runtimeType.toString()) {
     case 'AndroidDeviceInfo':
       AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
@@ -12,7 +15,7 @@ Future<String> getDeviceModel() async {
     case 'IosDeviceInfo':
       IosDeviceInfo iosInfo = await deviceInfo.iosInfo;
       return iosInfo.utsname.machine ?? baseDeviceInfoString;
-    case 'WebDeviceInfo':
+    case 'WebBrowserInfo':
       WebBrowserInfo webBrowserInfo = await deviceInfo.webBrowserInfo;
       // TODO: return userAgent?
       return webBrowserInfo.browserName
@@ -32,3 +35,159 @@ Future<Map<String, dynamic>> getDeviceInfoMap() async {
 // Push [map] to your service.
   return map;
 }
+
+// #region Platform Information
+String getPlatformString() {
+  return kIsWeb
+      ? 'Web Browser'
+      : Platform.isAndroid
+          ? 'Android Device'
+          : Platform.isIOS
+              ? 'iOS Device'
+              : Platform.isLinux
+                  ? 'Linux Device'
+                  : Platform.isMacOS
+                      ? 'MacOS Device'
+                      : Platform.isWindows
+                          ? 'Windows Device'
+                          : '';
+}
+
+Future<Map<String, dynamic>> getPlatformInfo() async {
+  final DeviceInfoPlugin deviceInfoPlugin = DeviceInfoPlugin();
+  Map<String, dynamic> deviceData = <String, dynamic>{};
+  try {
+    if (kIsWeb) {
+      deviceData = _readWebBrowserInfo(await deviceInfoPlugin.webBrowserInfo);
+    } else {
+      if (Platform.isAndroid) {
+        deviceData = _readAndroidBuildData(await deviceInfoPlugin.androidInfo);
+      } else if (Platform.isIOS) {
+        deviceData = _readIosDeviceInfo(await deviceInfoPlugin.iosInfo);
+      } else if (Platform.isLinux) {
+        deviceData = _readLinuxDeviceInfo(await deviceInfoPlugin.linuxInfo);
+      } else if (Platform.isMacOS) {
+        deviceData = _readMacOsDeviceInfo(await deviceInfoPlugin.macOsInfo);
+      } else if (Platform.isWindows) {
+        deviceData = _readWindowsDeviceInfo(await deviceInfoPlugin.windowsInfo);
+      }
+    }
+  } on PlatformException {
+    deviceData = <String, dynamic>{'Error:': 'Failed to get platform version.'};
+  }
+  return deviceData;
+}
+
+// #endregion
+
+// #region DeviceInfoMethods
+
+Map<String, dynamic> _readAndroidBuildData(AndroidDeviceInfo build) {
+  return <String, dynamic>{
+    'version.securityPatch': build.version.securityPatch,
+    'version.sdkInt': build.version.sdkInt,
+    'version.release': build.version.release,
+    'version.previewSdkInt': build.version.previewSdkInt,
+    'version.incremental': build.version.incremental,
+    'version.codename': build.version.codename,
+    'version.baseOS': build.version.baseOS,
+    'board': build.board,
+    'bootloader': build.bootloader,
+    'brand': build.brand,
+    'device': build.device,
+    'display': build.display,
+    'fingerprint': build.fingerprint,
+    'hardware': build.hardware,
+    'host': build.host,
+    'id': build.id,
+    'manufacturer': build.manufacturer,
+    'model': build.model,
+    'product': build.product,
+    'supported32BitAbis': build.supported32BitAbis,
+    'supported64BitAbis': build.supported64BitAbis,
+    'supportedAbis': build.supportedAbis,
+    'tags': build.tags,
+    'type': build.type,
+    'isPhysicalDevice': build.isPhysicalDevice,
+    'androidId': build.androidId,
+    'systemFeatures': build.systemFeatures,
+  };
+}
+
+Map<String, dynamic> _readIosDeviceInfo(IosDeviceInfo data) {
+  return <String, dynamic>{
+    'name': data.name,
+    'systemName': data.systemName,
+    'systemVersion': data.systemVersion,
+    'model': data.model,
+    'localizedModel': data.localizedModel,
+    'identifierForVendor': data.identifierForVendor,
+    'isPhysicalDevice': data.isPhysicalDevice,
+    'utsname.sysname:': data.utsname.sysname,
+    'utsname.nodename:': data.utsname.nodename,
+    'utsname.release:': data.utsname.release,
+    'utsname.version:': data.utsname.version,
+    'utsname.machine:': data.utsname.machine,
+  };
+}
+
+Map<String, dynamic> _readLinuxDeviceInfo(LinuxDeviceInfo data) {
+  return <String, dynamic>{
+    'name': data.name,
+    'version': data.version,
+    'id': data.id,
+    'idLike': data.idLike,
+    'versionCodename': data.versionCodename,
+    'versionId': data.versionId,
+    'prettyName': data.prettyName,
+    'buildId': data.buildId,
+    'variant': data.variant,
+    'variantId': data.variantId,
+    'machineId': data.machineId,
+  };
+}
+
+Map<String, dynamic> _readWebBrowserInfo(WebBrowserInfo data) {
+  return <String, dynamic>{
+    'browserName': describeEnum(data.browserName),
+    'appCodeName': data.appCodeName,
+    'appName': data.appName,
+    'appVersion': data.appVersion,
+    'deviceMemory': data.deviceMemory,
+    'language': data.language,
+    'languages': data.languages,
+    'platform': data.platform,
+    'product': data.product,
+    'productSub': data.productSub,
+    'userAgent': data.userAgent,
+    'vendor': data.vendor,
+    'vendorSub': data.vendorSub,
+    'hardwareConcurrency': data.hardwareConcurrency,
+    'maxTouchPoints': data.maxTouchPoints,
+  };
+}
+
+Map<String, dynamic> _readMacOsDeviceInfo(MacOsDeviceInfo data) {
+  return <String, dynamic>{
+    'computerName': data.computerName,
+    'hostName': data.hostName,
+    'arch': data.arch,
+    'model': data.model,
+    'kernelVersion': data.kernelVersion,
+    'osRelease': data.osRelease,
+    'activeCPUs': data.activeCPUs,
+    'memorySize': data.memorySize,
+    'cpuFrequency': data.cpuFrequency,
+    'systemGUID': data.systemGUID,
+  };
+}
+
+Map<String, dynamic> _readWindowsDeviceInfo(WindowsDeviceInfo data) {
+  return <String, dynamic>{
+    'numberOfCores': data.numberOfCores,
+    'computerName': data.computerName,
+    'systemMemoryInMegabytes': data.systemMemoryInMegabytes,
+  };
+}
+
+// #endregion
--- lib/page/InfoPage.dart
+++ lib/page/InfoPage.dart
@@ -2,11 +2,34 @@ import 'package:flutter/material.dart';
 import 'package:toolbox/implementation/DeviceInfoPlus.dart';
 import 'package:toolbox/widget/NavigationDrawerWidget.dart';
 
-class InfoPage extends StatelessWidget {
+class InfoPage extends StatefulWidget {
   const InfoPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) async {
+  State<InfoPage> createState() => _InfoPageState();
+}
+
+class _InfoPageState extends State<InfoPage> {
+  Map<String, dynamic> _deviceData = <String, dynamic>{};
+
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  Future<void> initPlatformState() async {
+    final Map<String, dynamic> deviceData = await getPlatformInfo();
+
+    if (!mounted) return;
+
+    setState(() {
+      _deviceData = deviceData;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       drawer: const NavigationDrawerWidget(),
       appBar: AppBar(
@@ -17,11 +40,50 @@ class InfoPage extends StatelessWidget {
       body: Center(
         child: Column(
           children: [
-            Title(color: Colors.black, child: const Text("Toolbox")),
-            Text(getDeviceModel()))
-            const Text(
-              "Version: 1.0.0",
-              style: TextStyle(fontSize: 20),
+            const SelectableText(
+              "Toolbox",
+              style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold),
+            ),
+            const Text("Version: 1.0.0"),
+            Text("Device: ${getPlatformString()}"),
+            const Divider(height: 10),
+            const Text("Device Info",
+                style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
+            ListView(
+              shrinkWrap: true,
+              padding: const EdgeInsets.all(20),
+              children:
+                  _deviceData.entries.map((MapEntry<String, dynamic> entry) {
+                String key = entry.key;
+                String value = entry.value.toString();
+
+                key = key.replaceAllMapped(RegExp(r'[A-Z]'), (match) {
+                  return " " + (match.group(0) ?? "");
+                });
+                key = "${key[0].toUpperCase()}${key.substring(1)}";
+
+                return Row(
+                  children: [
+                    Expanded(
+                      child: Wrap(children: [
+                        Text(key,
+                            style:
+                                const TextStyle(fontWeight: FontWeight.bold)),
+                        const Text(": "),
+                        Text(value),
+                      ]),
+
+                      // Text(
+                      //   '${entry.key}: ${entry.value}',
+                      //   style: const TextStyle(fontWeight: FontWeight.bold),
+                      // ),
+                    ),
+                  ],
+                );
+              }).toList(),
             ),
           ],
         ),
